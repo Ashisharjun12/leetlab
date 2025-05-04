47d008558fe13638e6b6c0df04ccd7af05500ae0
Ashish Raj <ashishrahul748@gmail.com>
2025-05-05 00:18:42 +0530

    db initilization and folder setup

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,13 @@
+import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core';
 
+export const userRoles = pgEnum('user_roles', ['user', 'admin']);
 
-
-
-
+export const user = pgTable('users', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    name: text('name').notNull(),
+    email: text('email').notNull().unique(),
+    password: text('password').notNull(),
+    role: userRoles('role').notNull().default('user'),
+    createdAt: timestamp('created_at').defaultNow(),
+    updatedAt: timestamp('updated_at').defaultNow(),
+});
